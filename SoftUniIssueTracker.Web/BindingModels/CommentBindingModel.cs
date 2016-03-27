d0f3a0860f90b75a5dbd27001d22a15c4721d8c6
Ivelin Kirilov <ivelinkv@gmail.com>
2016-03-27 14:37:08 +0300

    add / get comment/s for issue

---
+++ SoftUniIssueTracker.Web/BindingModels/CommentBindingModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SIT.Models;
+
+namespace SIT.Web.BindingModels
+{
+    public class CommentBindingModel
+    {
+        public string Text { get; set; }
+        public int IssueId { get; set; }
+    }
+}
